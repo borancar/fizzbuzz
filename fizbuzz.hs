c239e763ab0acb23adbeb54528da3045de666319
Boran Car <boran.car@gmail.com>
2018-09-12 15:41:30 +0100

    Add Fizz, Buzz and FizzBuzz rules

--- fizbuzz.hs
+++ fizbuzz.hs
@@ -1,12 +1,30 @@
 import Control.Monad
 import Data.Maybe
 
+fizzRule :: Integer -> Maybe String
+fizzRule i =
+    if i `mod` 3 == 0
+        then Just "Fizz"
+        else Nothing
+
+buzzRule :: Integer -> Maybe String
+buzzRule i =
+    if i `mod` 5 == 0
+        then Just "Buzz"
+        else Nothing
+
+fizzBuzzRule :: Integer -> Maybe String
+fizzBuzzRule i =
+    if i `mod` 3 == 0 && i `mod` 5 == 0
+        then Just "FizzBuzz"
+        else Nothing
+
 printRule :: Integer -> Maybe String
 printRule i = Just $ show i
 
 evaluateRules :: Integer -> String
 evaluateRules i = fromJust $ msum $ sequence rules i where
-    rules = [printRule]
+    rules = [fizzBuzzRule, fizzRule, buzzRule, printRule]
 
 main = do
     mapM_ (putStrLn . evaluateRules) [1..100]
