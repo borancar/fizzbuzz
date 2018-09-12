69e04a970bca67b229ccea0eb1196eca0ac62024
Boran Car <boran.car@gmail.com>
2018-09-12 15:54:09 +0100

    Flamingo and Pink Flamingo rules

--- fizbuzz.hs
+++ fizbuzz.hs
@@ -19,12 +19,31 @@ fizzBuzzRule i =
         then Just "FizzBuzz"
         else Nothing
 
+-- from https://stackoverflow.com/questions/2807686/whats-the-way-to-determine-if-an-int-is-a-perfect-square-in-haskell
+-- importing a library seemed too much for now so just going to use this (which was incidentally my original idea)
+isSquare :: Integer -> Bool
+isSquare n = sq * sq == n where
+    sq = floor $ sqrt $ (fromIntegral n::Double)
+
+flamingoRule :: Integer -> Maybe String
+flamingoRule i =
+    if isSquare (5 * i*i + 4) || isSquare (5 * i*i - 4)
+        then Just "Flamingo"
+        else Nothing
+
+pinkFlamingoRule :: Integer -> Maybe String
+pinkFlamingoRule i =
+    if (isSquare (5 * i*i + 4) || isSquare (5 * i*i - 4)) &&
+    i `mod` 3 == 0 && i `mod` 5 == 0
+        then Just "Pink Flamingo"
+        else Nothing
+
 printRule :: Integer -> Maybe String
 printRule i = Just $ show i
 
 evaluateRules :: Integer -> String
 evaluateRules i = fromJust $ msum $ sequence rules i where
-    rules = [fizzBuzzRule, fizzRule, buzzRule, printRule]
+    rules = [pinkFlamingoRule, flamingoRule, fizzBuzzRule, fizzRule, buzzRule, printRule]
 
 main = do
     mapM_ (putStrLn . evaluateRules) [1..100]
